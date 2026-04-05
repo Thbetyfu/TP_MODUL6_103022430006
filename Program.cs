c3755be530199bf3ffd9e08cff41ffe97c3d0785
Thbetyfu <thoriq.a.taqy@gmail.com>
2026-04-05 11:39:11 +0800

    Menambahkan erro handling try catch (desain by contrak)

--- Program.cs
+++ Program.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using TP_MODUL6_103022430006;
 
-namespace TP_MODUL6_103022430006
+namespace TP_MODUL6_NIM
 {
     class Program
     {
         static void Main(string[] args)
         {
-            SayaMusicTrack laguThoriq = new SayaMusicTrack("GENJER-GENJER - Justin Bieber");
-            laguThoriq.IncreasePlayCount(500);
-            laguThoriq.PrintTrackDetails();
+            Console.WriteLine("--- Test 1: Penambahan Normal ---");
+            SayaMusicTrack laguNormal = new SayaMusicTrack("Ghost - Justin Bieber");
+            laguNormal.IncreasePlayCount(5000000);
+            laguNormal.PrintTrackDetails();
 
-            Console.WriteLine("Tekan sembarang tombol untuk keluar...AHIHIHI");
+            //pengujian 2: Memicu Overflow
+            Console.WriteLine("\n--- Test 2: Memicu Overflow ---");
+            for (int i = 0; i < 220; i++)
+            {
+                laguNormal.IncreasePlayCount(10000000);
+            }
+            laguNormal.PrintTrackDetails();
+
+            // PENGUJIAN 3: Memicu Precondition
+            Console.WriteLine("\nProgram selesai. Tekan sembarang tombol untuk keluar...");
             Console.ReadKey();
         }
     }
---
+++ SayaMusikTrack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TP_MODUL6_103022430006
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Debug.Assert(title != null, "Judul tidak boleh null!");
+            Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter!");
+            this.title = title;
+            Random random = new Random();
+            this.id = random.Next(10000, 100000);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            Debug.Assert(count <= 10000000, "Input penambahan maksimal 10.000.000!");
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Play count sudah melebihi batas maksimum integer (Overflow)!");
+            }
+           }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"ID: {this.id} | Title: {this.title} | Plays: {this.playCount}");
+        }
+    }
+}

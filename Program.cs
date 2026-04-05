a0f321196730ba42e0d87c0981ac7c00f9187f91
Thbetyfu <thoriq.a.taqy@gmail.com>
2026-04-05 11:19:50 +0800

    mengerjakan class sayamusuiktrack dengan lagu genjer genjer by justin bieber

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL6_103022430006;
+
+namespace TP_MODUL6_103022430006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack laguThoriq = new SayaMusicTrack("GENJER-GENJER - Justin Bieber");
+            laguThoriq.IncreasePlayCount(500);
+            laguThoriq.PrintTrackDetails();
+
+            Console.WriteLine("Tekan sembarang tombol untuk keluar...AHIHIHI");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SayaMusikTrack - Copy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TP_MODUL6_103022430006
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+            Random random = new Random();
+            this.id = random.Next(10000, 100000);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("Detail Lagu:");
+            Console.WriteLine($"ID         : {this.id}");
+            Console.WriteLine($"Judul      : {this.title}");
+            Console.WriteLine($"Play Count : {this.playCount}");
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
